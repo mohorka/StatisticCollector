72d0e4e7885687e5ca0506f08b089f463efd2465
Victoria Dzyuba <47743417+mohorka@users.noreply.github.com>
2020-05-10 14:15:46 +0300

    update create page
    add ability to work with multilingual text

--- Pages/UserDictionaries/Create.cshtml.cs
+++ Pages/UserDictionaries/Create.cshtml.cs
@@ -16,12 +16,16 @@ namespace StatisticCollector.Pages.UserDictionaries
       
         private List<string> newWords { get; set; }
         private List<string> languages { get; set; }
+        private string language { get; set; }
         private List<SingleWord> wordsToAdd = new List<SingleWord>();
+        private Dictionary<string, uint> dictionary = new Dictionary<string, uint>();
         private IQueryable<SingleWord> wordsInDb { get; set; }
         public DetectLanguageService languageService = new DetectLanguageService();
 
         [BindProperty]
         public string text { get; set; }
+        [BindProperty]
+        public bool isChecked { get; set; }
 
         public CreateModel(ApplicationContext db) => _context = db;
         public void OnGet()
@@ -32,32 +36,63 @@ namespace StatisticCollector.Pages.UserDictionaries
             if (ModelState.IsValid)
             {
                 try
-                {   
-                    newWords = ParserService.Parse(text);
-                    wordsToAdd.AddWords(newWords);
-                    languages = languageService.GetLanguages(newWords);
-                    foreach(string language in languages)
+                {
+                    if (isChecked)
                     {
+                        newWords = ParserService.Parse(text);
+                        wordsToAdd.AddWords(newWords);
+                        languages = languageService.GetLanguages(newWords);
+                        foreach (string language in languages)
+                        {
+                            wordsInDb = _context.Words.Where(x => x.Language == language);
+                            if (wordsInDb == null)
+                            {
+                                _context.AddRange(wordsToAdd);
+                            }
+                            else
+                            {
+                                foreach (SingleWord word in wordsToAdd)
+                                {
+                                    var wordToUpdate = wordsInDb.FirstOrDefault(x => x.Word == word.Word);
+                                    if (wordToUpdate != null)
+                                    {
+                                        _context.Words.Find(wordToUpdate.Id).Frequency += word.Frequency;
+                                    }
+                                    else
+                                        _context.Add(word);
+                                }
+
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        newWords = ParserService.Parse(text);
+                        language = languageService.GetLanguage(text);
+                        dictionary.AddWords(newWords);
                         wordsInDb = _context.Words.Where(x => x.Language == language);
+
                         if (wordsInDb == null)
                         {
-                            _context.AddRange(wordsToAdd);
+                            _context.AddRange(dictionary.DictionaryToSingleWords(language));
+
                         }
                         else
                         {
-                            foreach(SingleWord word in wordsToAdd)
+                            foreach (SingleWord word in dictionary.DictionaryToSingleWords(language))
                             {
                                 var wordToUpdate = wordsInDb.FirstOrDefault(x => x.Word == word.Word);
-                                if (wordToUpdate != null)
+                                if (wordToUpdate == null)
+                                {
+                                    _context.Add(word);
+                                }
+                                else
                                 {
                                     _context.Words.Find(wordToUpdate.Id).Frequency += word.Frequency;
                                 }
-                                else
-                                    _context.Add(word);
                             }
-                      
                         }
-                        
                     }
 
 
